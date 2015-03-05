974d31c335e3e0b2dcd1d5e90d1066ef48adc391
rbeti <rbeti@hsr.ch>
2015-03-05 23:40:16 +0100

    Added function to embed YouTube videos.

--- CSQ_SA/js/quizzenger.js
+++ CSQ_SA/js/quizzenger.js
@@ -1,4 +1,26 @@
 // Quizzenger JavaScript.
 var quizzenger = {
-	
+	embed : {
+		embedYouTube : function(url) {
+			var regex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+			var matches = url.match(regex);
+
+			if(!matches || matches[2].length != 11) {
+				return "";
+			}
+
+			return $('<div>', { 'class' :'video-container' })
+				.append($('<iframe>', {
+					'src' : 'https://www.youtube.com/embed/' + matches[2],
+					'frameborder' : '0',
+					'allowfullscreen' : ''
+				}));
+		}
+	},
+
+	markdown : {
+		generate : function(markdown) {
+
+		}
+	}
 };
