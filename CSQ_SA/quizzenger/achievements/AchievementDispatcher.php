e37dbb6ff2b84f08bec145218ff05bf8768f108b
rbeti <rbeti@hsr.ch>
2015-03-11 23:14:25 +0100

    Added base classes for achievement processing.

---
+++ CSQ_SA/quizzenger/achievements/AchievementDispatcher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace quizzenger\achievements {
+	use \mysqli as mysqli;
+
+	class AchievementDispatcher {
+		public function __construct(mysqli $mysqli) {
+			//
+		}
+	} // class AchievementDispatcher
+} // namespace quizzenger\achievements
+
+?>
---
+++ CSQ_SA/quizzenger/achievements/IAchievement.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace quizzenger\achievements {
+	use \mysqli as mysqli;
+
+	/**
+	 * Defines the plugin interface for individual achievements.
+	**/
+	interface IAchievement {
+		/**
+		 * This function determines whether the conditions have been met to
+		 * grant the specific achievement that implements this interface.
+		 * @param mysqli $database Connection to the database.
+		 * @param string $event Event that caused the function to be called.
+		 * @param object $arguments List of arguments defined for the achievement.
+		 * @return boolean Returns true if the specific achievement is to be granted, false otherwise.
+		**/
+		public function grant(mysqli $database, $event, $arguments)
+	} // interface IAchievement
+} // namespace quizzenger\achievements
+
+?>
