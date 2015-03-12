e390bc654fe997cb1a2b8d07f4ab81f4cf481c5a
szingg <szingg@hsr.ch>
2015-03-12 19:50:39 +0100

    save arguments as array with key-value pairs

--- CSQ_SA/installerAchievements.php
+++ CSQ_SA/installerAchievements.php
@@ -1,4 +1,25 @@
 <?php
+
+function throwXmlParseException(){ 
+	echo("<p style=\"color:red; \">achievements.xml could not successfully be parsed.</p><br>");
+	die();
+}
+
+function throwVersionException($version){
+	echo("<p style=\"color:red; \">Version conflict. Expected Version is '".$version."' </p><br>");
+	die();
+}
+
+function throwMalformedXMLException(){
+	echo("<p style=\"color:red; \">Parse Error: Malformed XML</p><br>");
+	die();
+}
+
+function throwSqlInsertException($link){
+	echo("<p style=\"color:red; \">Insert Failed! Error : (". $link->errno .") ". $link->error ."</p><br>");
+	die();
+}
+
 include("includes/config.php");
 if(isset($_POST['install'])){
 
@@ -18,76 +39,58 @@ if(isset($_POST['install'])){
 	echo("<p style=\"color:green;\">Connected to DB ".db."</p><br>");
 
 	
-	$sqlErrorCode=0;
-	/*
-	$sqlFileToExecute='./install.sql';
-	// read the sql file
-	$f = fopen($sqlFileToExecute,"r");
-	$sqlFile = fread($f, filesize($sqlFileToExecute));
-	fclose($f);
-	$sqlArray = explode(';',$sqlFile);
-	foreach ($sqlArray as $stmt) {
-		if (strlen($stmt)>3 && substr(ltrim($stmt),0,2)!='/*') {
-			$result = mysqli_query($link,$stmt);
-			if (!$result) {
-				$sqlErrorCode = mysql_errno();
-				$sqlErrorText = mysql_error();
-				$sqlStmt = $stmt;
-				break;
-			}
-		}
-	}
-	*/
+
 	$xml = simplexml_load_file('achievements.xml');
 	//check version
 	if(!isset($xml['version'])){
-		echo("<p style=\"color:red;\">achievements.xml could not successfully be parsed.</p><br>");
-		die();
+		throwXmlParseException();
 	} 
 	$version = '2.0';
 	if($xml['version'] != $version){
-		echo("<p style=\"color:red;\">Version conflict. Expected Version is '".$version."' </p><br>");
-		die();
+		throwVersionException($version);
 	}
 	foreach ($xml->achievements[0]->achievement as $ach){
+		if(! isset($ach['type'], $ach->name, $ach->order, $ach->description, $ach->image, $ach->arguments, $ach->bonusscore, $ach->triggers)){
+			throwMalformedXMLException();
+		}
 		$type = '"'.$link->real_escape_string($ach['type']).'"';
 		$name = '"'.$link->real_escape_string($ach->name).'"';
 		$order = '"'.$link->real_escape_string($ach->order).'"';
 		$description = '"'.$link->real_escape_string($ach->description).'"';
 		$image = '"'.$link->real_escape_string($ach->image).'"';
-		$arguments = '"'.$link->real_escape_string($ach->arguments->asXML()).'"';
-		$args1 = $ach->arguments->asXML();
+		//$arguments = '"'.$link->real_escape_string($ach->arguments->asXML()).'"';
+		//$argumentsJSON = json_decode(json_encode((array) $ach->arguments), 1);
+		$arguments = array();
+		foreach($ach->arguments->argument as $arg){
+			if(! isset($arg["name"], $arg["value"])){
+				throwMalformedXMLException();
+			}
+			$arguments[$link->real_escape_string($arg["name"])] = $link->real_escape_string($arg["value"]);
+		}
+		$arguments = '"'.$link->real_escape_string(json_encode($arguments)).'"'; //decode with json_decode($arguments, true);
 		$bonusscore = '"'.$link->real_escape_string($ach->bonusscore).'"';
+		$triggers = $ach->triggers;
 		
-		$result = $link->query("INSERT INTO achievement (name, description, type, image, attributes, bonus_score) VALUES ($name, $description, $type, $image, $arguments, $bonusscore)");
+		$result = $link->query("INSERT INTO achievement (name, description, type, image, arguments, bonus_score) VALUES ($name, $description, $type, $image, $arguments, $bonusscore)");
 		if($result){
 			$achievementId = $link->insert_id;
-			foreach($ach->triggers->trigger as $trigger){
+			foreach($triggers->trigger as $trigger){
+				if(! isset($trigger['name'], $achievementId)){
+					echo('<p style=\"color:red;\">Parse Error: Malformed XML</p><br>');
+					die();
+				}
 				$triggerName = '"'.$link->real_escape_string($trigger['name']).'"';
-				$resultTrigger = $link->query("INSERT INTO achievementtrigger (achievement_id, type) VALUES ($achievementId, $triggerName)");
+				$resultTrigger = $link->query("INSERT INTO achievementtrigger (achievement_id, name) VALUES ($achievementId, $triggerName)");
 				if($resultTrigger){}
 				else{
-					echo('<p style=\"color:red;\">Insert Failed! Error : ('. $link->errno .') '. $link->error .'</p><br>');
-					die();
+					throwSqlInsertException($link);
 				}
 			}	
 		}
 		else{
-			echo('<p style=\"color:red;\">Insert Failed! Error : ('. $link->errno .') '. $link->error .'</p><br>');
-			die();
+			throwSqlInsertException($link);
 		}
-		//$achId = mysqli->s_insert("INSERT INTO question (type, questiontext, user_id, category_id,created,attachment,attachment_local) VALUES (?, ?, ?, ?, ?, ?, ?)",array('s', 's','i','i','s','s','i'),array($type,$questiontext,$userID,$categoryID,null,$attachment,$attachment_local));
 		
-	}
-	if ($sqlErrorCode == 0) {
-		echo("<p style=\"color:green;\">Finished successfully!</p><br>");
-	} else {
-		echo("<p style=\"color:red\">");
-			echo "An error occured during installation!<br/>";
-			echo "Error code: " . htmlspecialchars($sqlErrorCode) . "<br/>";
-			echo "Error text: " . htmlspecialchars($sqlErrorText) . "<br/>";
-			echo "Statement:<br/> " . htmlspecialchars($sqlStmt) . "<br/>";
-		echo("</p>");
 	}
 	echo("<hr>");
 	echo"<b>Remove following files: achievements.xml, installerAchievements.php!</b>";
