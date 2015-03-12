646812108d98790c63b295458a882608cea7edea
rbeti <rbeti@hsr.ch>
2015-03-12 10:51:36 +0100

    Added empty class 'ScoreDispatcher'.

---
+++ CSQ_SA/quizzenger/scoring/ScoreDispatcher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace quizzenger\scoring {
+	use \mysqli as mysqli;
+
+	class ScoreDispatcher {
+		public function __construct(mysqli $mysqli) {
+			//
+		}
+	} // class ScoreDispatcher
+} // namespace quizzenger\scoring
+
+?>
