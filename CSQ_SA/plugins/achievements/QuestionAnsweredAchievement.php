edacd6798c530291f769e757154608b366b56ee2
rbeti <rbeti@hsr.ch>
2015-03-13 22:00:07 +0100

    Added first part of the achievement plugin.

---
+++ CSQ_SA/plugins/achievements/QuestionAnsweredAchievement.php
@@ -0,0 +1,11 @@
+<?php
+	namespace quizzenger\plugins\achievements {
+		use \quizzenger\achievements\IAchievement as IAchievement;
+
+		class QuestionAnsweredAchievement implements IAchievement {
+			public function grant() {
+				//	
+			}
+		} // class QuestionAnsweredAchievement
+	} // namespace quizzenger\plugins\achievements
+?>
