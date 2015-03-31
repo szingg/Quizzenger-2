21a843e752aa4716c6ead4a8b78189c3d6f28297
rbeti <rbeti@hsr.ch>
2015-03-31 12:14:10 +0200

    Added static EventController class that simplifies event dispatching.

---
+++ CSQ_SA/quizzenger/controlling/EventController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace quizzenger\controlling {
+	use \mysqli as mysqli;
+	use \quizzenger\dispatching\UserEvent as UserEvent;
+	use \quizzenger\dispatching\EventDispatcher as EventDispatcher;
+
+	class EventController {
+		private static $dispatcher;
+
+		public static function setup(mysqli $mysqli) {
+			self::$dispatcher = new \EventDispatcher($mysqli);
+		}
+
+		public static function fire($name, $userId, array $arguments = []) {
+			$event = new UserEvent($name, $userId);
+			foreach($arguments as $key => $value) {
+				$event->set($key, $value);
+			}
+
+			return self::$dispatcher->fire($event);
+		}
+	} // class EventController
+} // namespace quizzenger\controlling
+
+?>
