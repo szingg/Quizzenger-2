d4a46212f5b058c67d07d3d1e354f1556fa9bcba
rbeti <rbeti@hsr.ch>
2015-04-02 18:23:37 +0200

    Added NavigationUtility class.

---
+++ CSQ_SA/quizzenger/utilities/NavigationUtility.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace quizzenger\utilities {
+	class NavigationUtility {
+		private function __construct() {
+			//
+		}
+
+		public static function redirectToErrorPage() {
+			// TODO.
+		}
+	} // class NavigationUtility
+} // namespace quizzenger\utilities
+
+?>
