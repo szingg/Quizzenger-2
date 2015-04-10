7c03fddd1c67656ffdf0a987028459292e276d4d
rbeti <rbeti@hsr.ch>
2015-04-10 15:16:00 +0200

    Implemented format parser and replacer.

--- CSQ_SA/quizzenger/messages/MessageFormatter.php
+++ CSQ_SA/quizzenger/messages/MessageFormatter.php
@@ -16,7 +16,9 @@ namespace quizzenger\messages {
 		private function resetOptions() {
 			$this->options = [
 				'unknown' => '????',
-				'precision' => 2
+				'precision' => 2,
+				'decimal' => '.',
+				'thousands' => '\''
 			];
 		}
 
@@ -24,8 +26,51 @@ namespace quizzenger\messages {
 			$this->options = array_merge($this->options, $merge);
 		}
 
+		private function formatUnknownError($hint) {
+			return $this->options['unknown'] . $hint . $this->options['unknown'];
+		}
+
+		private function replace($name, $type, $arguments) {
+			if(!array_key_exists($name, $arguments))
+				return $this->formatUnknownError('name');
+
+			$value = $arguments[$name];
+			switch(mb_strtolower($type)) {
+				case 'string':
+					return (string)$value;
+
+				case 'integer':
+					return (string)(int)$value;
+
+				case 'double':
+					return number_format($value, $this->options['precision'], $this->options['decimal'], $this->options['thousands']);
+
+				default:
+					return $this->formatUnknownError('type');
+			}
+		}
+
 		public function format($input, array $arguments) {
-			return $input;
+			$matches = [];
+			$offset = 0;
+			$output = '';
+			while(preg_match('/\{\{(\w+)(?:#([\w\d_-]+))?\}\}/', $input, $matches, PREG_OFFSET_CAPTURE, $offset)) {
+				$name = $matches[1][0];
+				$type = (isset($matches[2][0]) ? $matches[2][0] : 'string');
+
+				$matchStart = $matches[0][1];
+				$matchLength = strlen($matches[0][0]);
+
+				// Add part between last match and current match.
+				$output .= substr($input, $offset, $matchStart - $offset);
+
+				// Replace the match with the associated value.
+				$output .= $this->replace($name, $type, $arguments);
+
+				$offset = $matchStart + $matchLength;
+			}
+			$output .= substr($input, $offset);
+			return $output;
 		}
 	} // class MessageQueue
 } // namespace quizzenger\messages
