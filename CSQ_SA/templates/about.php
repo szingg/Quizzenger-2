7087c9baceb8af6cb5091dd5fe27730be2fecd2e
rbeti <rbeti@hsr.ch>
2015-04-22 13:59:38 +0200

    Updated site text in about template.

--- CSQ_SA/templates/about.php
+++ CSQ_SA/templates/about.php
@@ -11,20 +11,29 @@
 		<p>Die Fragen werden entweder unterwegs auf einem Smartphone oder auf einem PC in einem Webbrowser beantwortet. Die Mobileseite ist für das Konsumieren des Inhaltes gedacht, die Desktopseite bietet zusätzliche Funktionen wie neue Fragen zu erfassen und gewisse administrative Tätigkeiten zu übernehmen
 		Als Fragetypen ist im Rahmen unseres Projektes Single Choice Fragen gedacht.</p>
 		<p>Bei der Architektur und Implementation wird viel Wert auf Flexibilität gelegt, sodass das Projekt ohne grossen Aufwand erweitert werden kann.</p>
-  	</div>
-  	<div class="col-md-6">
+
 	  	<h3>Ziel</h3>
 		<p>Unser Ziel ist es einerseits eine erfolgreiche und spannende Studienarbeit durchzuführen, andererseits eine gut zu wartende Lernplattform zu erstellen.
 		Diese Lernplattform soll sich mit der Zeit selbst verwalten und den Leuten eine effektive Möglichkeit bieten zu Lernen sowie eigenes Wissen weiterzugeben.</p>
 		<p>Wir versuchen unser Bestes eine stabile und gut durchdachte Plattform zu erstellen, auf der im Rahmen einer weiteren Arbeit weitere Features und Verbesserunge eingebracht werden können.</p>
-
+	</div>
+	<div class="col-md-6">
 		<h3>Technisches</h3>
 		<p>Verwendet wurde PHP 5.5, MySQLi, Eclipse PDT, EGIT.</p>
 		<p>Der Webserver zum Testen war ein Debian 3.2 vServer mit Apache2 und MySQL 5.6.</p>
-		<p>Link zum <a href="http://www.github.com">GitHub Repository</a></p>
+		<p>Link zum <a href="https://github.com/ozzi-/Quizzenger/">GitHub Repository</a></p>
+
+		<h3>Herausgeberin</h3>
+		<p>HSR Hochschule für Technik Rapperswil<br>Oberseestrasse 10, Postfach 1475, CH-8640 Rapperswil<br>Tel: 055 222 41 11, Fax: 055 222 44 00, office(at)hsr.ch</p>
+		<p>Diese Website wird durch die Abteilung Informatik der HSR Hochschule für Technik Rapperswil betreut. Fragen und Anregungen nehmen wir gerne entgegen.</p>
+		<p>Quizzenger wurde von Studierenden im Rahmen von Studienarbeiten erstellt und befindet sich noch in der Entwicklung. Für Probleme, die durch die Nutzung des Systems entstehen, übernimmt die HSR keine Haftung. Die HSR hat zudem keine Kontrolle über den Inhalt der in das System eingegebenen Fragen. Bitte melden Sie unangemessene Inhalte und allfällige Probleme an elearning(at)hsr.ch.</p>
+
+		<h3>Rechtliche Hinweise</h3>
+		<p>Die HSR Hochschule für Technik Rapperswil behält sich das Recht vor, jederzeit und nach eigenem Ermessen den Inhalt der Website ganz oder teilweise zu ändern, zu ergänzen oder zu löschen. Trotz aller vernünftigerweise zumutbaren Vorkehrungen können technische Ungenauigkeiten, Auslassungen oder Fehler in den Inhalten der Website der HSR nicht ausgeschlossen werden. Die HSR kann keine Haftung für die Richtigkeit, Vollständigkeit und Aktualität der auf der Website publizierten Informationen übernehmen.
+		Manche Links auf der HSR Website führen zu Websites Dritter. Diese stehen nicht mehr unter dem Einflussbereich der HSR. Für die Richtigkeit, Vollständigkeit und Rechtmässigkeit der dort enthaltenen Inhalte und Verlinkung mit weiteren Websites übernimmt die HSR keinerlei Verantwortung.</p>
 	</div>
 </div>
 <br><hr>
-<img src="<?=htmlspecialchars(APP_PATH)?>/templates/img/header_s.png"  class="img-responsive center-block" alt="Quizzenger Header Logo" style"max-width:364 px;" />
+<img src="<?=htmlspecialchars(APP_PATH)?>/templates/img/header_s.png" class="img-responsive center-block" alt="Quizzenger Header Logo" />
 
 
