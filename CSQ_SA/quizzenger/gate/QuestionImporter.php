092faa2fe88b62cd4cec7402d146a3c55dc31ad8
rbeti <rbeti@hsr.ch>
2015-05-06 12:27:57 +0200

    Implemented receiving of all XML values.

--- CSQ_SA/quizzenger/gate/QuestionImporter.php
+++ CSQ_SA/quizzenger/gate/QuestionImporter.php
@@ -7,9 +7,23 @@ namespace quizzenger\gate {
 
 	class QuestionImporter {
 		private $mysqli;
+		private $questionInsertStatement;
+		private $answerInsertStatement;
 
 		public function __construct(mysqli $mysqli) {
 			$this->mysqli = $mysqli;
+
+			$this->questionInsertStatement = $this->mysqli->prepare('INSERT INTO question'
+				. ' (type, questiontext, user_id, created, lastModified, difficulty, category_id)'
+				. ' SELECT ?, ?, ?, ?, ?, ?, ct3.id'
+				. ' FROM category AS ct1'
+				. ' LEFT JOIN category AS ct2 ON ct2.parent_id=ct1.id'
+				. ' LEFT JOIN category AS ct3 ON ct3.parent_id=ct2.id'
+				. ' WHERE ct1.name=? AND ct2.name=? AND ct3.name=?');
+
+			$this->answerInsertStatement = $this->mysqli->prepare('INSERT INTO answer'
+				. ' (correctness, text, explanation, question_id)'
+				. ' VALUES (?, ?, ?, ?)');
 		}
 
 		private function transaction() {
@@ -28,12 +42,22 @@ namespace quizzenger\gate {
 			$this->mysqli->commit();
 		}
 
-		private function insertQuestion(SimpleXMLElement $question) {
-			// TODO: Implement insert statements.
+		private function insertQuestion($userId, SimpleXMLElement $question) {
+			$type = (string)$question->attributes()->type;
+			$difficulty = (string)$question->attributes()->difficulty;
+			$author = (string)$question->author;
+			$created = (string)$question->created;
+			$modified = (string)$question->modified;
+			$categoryFirst = (string)$question->category->first;
+			$categorySecond = (string)$question->category->second;
+			$categoryThird = (string)$question->category->third;
+			$text = (string)$question->text;
+
+			// TODO: Implement actual inserts.
 			return true;
 		}
 
-		public function import($data) {
+		public function import($userId, $data) {
 			$xml = simplexml_load_string($data);
 			if(!$xml) {
 				Log::error('Could not import questions from XML data.');
@@ -43,11 +67,12 @@ namespace quizzenger\gate {
 			$questions = $xml->xpath('/quizzenger-question-export/questions/question');
 			$this->transaction();
 			foreach($questions as $current) {
-				if(!$this->insertQuestion($current)) {
+				if(!$this->insertQuestion($userId, $current)) {
 					$this->rollback();
 					return false;
 				}
 			}
+
 			$this->commit();
 		}
 	} // class QuestionImporter
