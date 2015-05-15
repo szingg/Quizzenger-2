c284e980e97bdd50943cd0e45905626214b67528
szingg <szingg@hsr.ch>
2015-05-15 11:02:52 +0200

    fix install

--- CSQ_SA/install/install.sql
+++ CSQ_SA/install/install.sql
@@ -608,6 +608,6 @@ CREATE OR REPLACE VIEW rankinglistallcategoriesview AS
 --
 INSERT INTO category (id, name) VALUES (1, 'Strukturwissenschaften');
 INSERT INTO category (id, name,parent_id) VALUES (2, 'Informatik', 1);
-INSERT INTO category (id, name,parent_id) VALUES (3 'Wirtschaftsinformatik', 2);
+INSERT INTO category (id, name,parent_id) VALUES (3, 'Wirtschaftsinformatik', 2);
 
 INSERT INTO question (type, questiontext, user_id, category_id,created) VALUES ('SingleChoice', 'Bitte lösche mich nachdem eine ernsthafte Frage erstellt wurde.', 1, 3, null);
